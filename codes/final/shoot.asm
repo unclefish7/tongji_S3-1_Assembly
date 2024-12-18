054dcbae7ada9e2e5d9e37f229b3b1a8ff3c5b5a
Jerry-Desktop <unclefish7@outlook.com>
2024-12-18 18:02:15 +0800

    实现定时刷新，左右控制飞机移动

--- codes/final/shoot.asm
+++ codes/final/shoot.asm
@@ -2,11 +2,13 @@
 .STACK 100h
 
 PUBLIC clear_screen, print_string
-PUBLIC color, output_buffer         ; 现在我们自己定义这些变量
+PUBLIC color, output_buffer
 
 DATA SEGMENT
-    color DB 0Fh                    ; 白色前景色，黑色背景
-    output_buffer DB 'Hello, World!$'   ; 输出缓冲区
+    color DB 0Fh
+    output_buffer DB 'Press 4/6 to move, Q to quit$'    ; 修改提示文字
+    plane_char DB '^'              ; 飞机图标
+    plane_pos DW 2160             ; 飞机初始位置(第14行中间位置)
 DATA ENDS
 
 CODE SEGMENT
@@ -76,6 +78,118 @@ print_done:
     RET
 print_string ENDP
 
+; 显示飞机过程
+draw_plane PROC
+    PUSH AX
+    PUSH DI
+    PUSH ES
+
+    MOV AX, 0B800h
+    MOV ES, AX
+    MOV DI, plane_pos
+    
+    MOV AL, plane_char
+    MOV ES:[DI], AL
+    MOV AL, color
+    MOV ES:[DI+1], AL
+
+    POP ES
+    POP DI
+    POP AX
+    RET
+draw_plane ENDP
+
+; 处理输入过程 - 增加寄存器保护
+handle_input PROC
+    PUSH AX
+    PUSH BX
+    PUSH CX                 ; 保护更多寄存器
+    PUSH DX
+    PUSH SI
+    PUSH DI
+    PUSH ES
+    PUSHF                   ; 保存标志寄存器
+    
+    MOV AH, 1              
+    INT 16h
+    JZ input_done_pop      
+
+    MOV AH, 0              
+    INT 16h
+    
+    CMP AL, '4'            
+    JE move_left
+    CMP AL, '6'            
+    JE move_right
+    CMP AL, 'q'           
+    JE quit_game
+    JMP input_done_pop     
+
+move_left:
+    MOV BX, plane_pos      ; 使用BX而不是AX来存储和比较
+    CMP BX, 2080          
+    JLE input_done_pop     
+    SUB plane_pos, 2      
+    JMP input_done_pop
+
+move_right:
+    MOV BX, plane_pos      ; 使用BX而不是AX来存储和比较
+    CMP BX, 2238         
+    JGE input_done_pop    
+    ADD plane_pos, 2       
+    JMP input_done_pop
+
+quit_game:
+    POPF                    ; 恢复标志寄存器
+    POP ES
+    POP DI
+    POP SI
+    POP DX
+    POP CX
+    POP BX
+    POP AX
+    MOV AH, 4Ch
+    INT 21h
+
+input_done_pop:
+    POPF                    ; 恢复标志寄存器
+    POP ES
+    POP DI
+    POP SI
+    POP DX
+    POP CX
+    POP BX
+    POP AX
+    RET
+handle_input ENDP
+
+; 延时过程 - 增加寄存器保护
+delay PROC
+    PUSH AX
+    PUSH BX
+    PUSH CX
+    PUSH DX
+    PUSH SI                 ; 保护更多寄存器
+    PUSH DI
+    PUSH ES
+    PUSHF                   ; 保存标志寄存器
+    
+    MOV CX, 0h             
+    MOV DX, 4240h          
+    MOV AH, 86h
+    INT 15h                
+    
+    POPF                    ; 恢复标志寄存器
+    POP ES
+    POP DI
+    POP SI
+    POP DX
+    POP CX
+    POP BX
+    POP AX
+    RET
+delay ENDP
+
 ; 主程序
 START:
     MOV AX, DATA
@@ -84,17 +198,20 @@ START:
     ; 清屏
     CALL clear_screen
 
-    ; 设置显示位置并打印
-    MOV DI, 350                   ; 第二行开始位置
+game_loop:
+    CLI                     ; 禁用中断
+    CALL clear_screen
+    
+    MOV DI, 160            
     CALL print_string
-
-    ; 等待按键
-    MOV AH, 1
-    INT 21h
-
-    ; 退出程序
-    MOV AX, 4C00h
-    INT 21h
+    
+    CALL handle_input
+    CALL draw_plane
+    
+    STI                     ; 允许中断
+    CALL delay             ; 延时期间允许中断
+    
+    JMP game_loop
 
 CODE ENDS
 END START
